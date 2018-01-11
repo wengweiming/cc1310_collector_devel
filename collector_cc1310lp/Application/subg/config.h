dc67773f55696193fbea2fc2f6364809183e9bef
Aifrutech_ninghongye <ninghongye@aifrutech.com>
2018-01-11 09:13:48 +0800

    basic communication in 433MHz channel 1

--- collector_cc1310lp/Application/subg/config.h
+++ collector_cc1310lp/Application/subg/config.h
@@ -85,7 +85,7 @@ extern "C"
 #define CONFIG_MAC_SUPERFRAME_ORDER  15
 
 /*! Setting for Phy ID */
-#define CONFIG_PHY_ID                (APIMAC_STD_US_915_PHY_1)
+#define CONFIG_PHY_ID                (APIMAC_GENERIC_CHINA_433_PHY_128)     //(APIMAC_STD_US_915_PHY_1)
 
 #if ((CONFIG_PHY_ID >= APIMAC_MRFSK_STD_PHY_ID_BEGIN) && (CONFIG_PHY_ID <= APIMAC_MRFSK_STD_PHY_ID_END))
 /*! Setting for channel page */
@@ -104,13 +104,13 @@ extern "C"
  Reporting Interval - in milliseconds to be set on connected devices using
  configuration request messages
  */
-#define CONFIG_REPORTING_INTERVAL 90000
+#define CONFIG_REPORTING_INTERVAL 2000  //90000
 /*!
  Polling interval in milliseconds to be set on connected devices using
  configuration request messages. Must be greater than or equal to default
  polling interval set on sensor devices
  */
-#define CONFIG_POLLING_INTERVAL 6000
+#define CONFIG_POLLING_INTERVAL 500 //6000
 /*!
  Time interval in ms between tracking message intervals
  */
@@ -163,7 +163,7 @@ extern "C"
  APIMAC_STD_ETSI_863_PHY_3 (50kbps/2-FSK/863MHz band) has channels 0 - 33.
  APIMAC_GENERIC_CHINA_433_PHY_128 (50kbps/2-FSK/433MHz band) has channels 0 - 6.
 */
-#define CONFIG_CHANNEL_MASK           { 0x0F, 0x00, 0x00, 0x00, 0x00, 0x00, \
+#define CONFIG_CHANNEL_MASK           { 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, \
                                         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, \
                                         0x00, 0x00, 0x00, 0x00, 0x00 }
 /*!
